c42b86d154559512f6fef249fecd0eba531ce8ab
Ерканат Абдул-Мади <yerkanat_a@minplan.int>
2024-05-15 18:29:39 +0600

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import './App.css';
 import Button from './components/Button/Button';
 import CardButton from './components/CardButton/CardButton';
 import JournalItem from './components/JournalItem/JournalItem';
+import LeftPanel from './layouts/LeftPanel/LeftPanel';
+import Header from './components/Header/Header';
+import JournalList from './components/JournalList/JournalList';
+import JournalAddButton from './components/JournalAddButton/JournalAddButton';
+import Body from './layouts/Body/Body';
 
 
 export default function App(){
@@ -25,9 +30,19 @@ export default function App(){
   ];
 
   return (
-    <>
-    <h1>Project</h1>
-    <Button/>
+    <div className='app'>
+    
+    <LeftPanel>
+      <Header/>
+      <JournalAddButton/>
+      <JournalList>
+      <CardButton>
+    <JournalItem
+      title = {data[0].title}
+      date = {data[0].date}
+      text = {data[0].text}
+    />
+    </CardButton>
     <CardButton>
     <JournalItem
       title = {data[0].title}
@@ -35,11 +50,11 @@ export default function App(){
       text = {data[0].text}
     />
     </CardButton>
-    <JournalItem
-      title = {data[1].title}
-      date = {data[1].date}
-      text = {data[1].text}
-    />
-    </>
+      </JournalList>
+    </LeftPanel>
+    <Body>
+      Body 
+    </Body>
+    </div>
    );
 }
---
+++ src/components/JournalAddButton/JournalAddButton.jsx
@@ -0,0 +1,12 @@
+import CardButton from '../CardButton/CardButton';
+import './JournalAddButton.css';
+
+function JournalAddButton({children}) {
+    return (
+        <CardButton>
+            New record
+        </CardButton>
+    );
+}
+
+export default JournalAddButton;
