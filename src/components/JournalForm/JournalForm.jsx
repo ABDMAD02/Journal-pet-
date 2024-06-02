2ebad5d2d61d6882d30c1b89abecd4c7d4e92c43
Abdul-Madi <madi20.08@mail.ru>
2024-06-03 01:23:55 +0600

    commit

--- src/components/JournalForm/JournalForm.jsx
+++ src/components/JournalForm/JournalForm.jsx
@@ -1,13 +1,37 @@
+import { useState } from 'react';
 import Button from '../Button/Button';
 import './JournalForm.css';
 
 function JournalForm ({onSubmit}) {
 
+    const [formValidState, setFormValidState] = useState({
+        title: true,
+        text: true,
+        date:true 
+    });
+
     const addJournalItem = (e) => {
         e.preventDefault();
         const formData = new FormData(e.target);
         const formProps = Object.fromEntries(formData);
+        let isFormValid = true;
+        if (!formProps.title?.trim().length) {
+            setFormValidState(state => ({...state, title: false}));
+            isFormValid = false;
+        }
+        if (!formProps.text?.trim().length) {
+            setFormValidState(state => ({...state, text: false}));
+            isFormValid = false;
+        }
+        if (!formProps.date) {
+            setFormValidState(state => ({...state, date: false}));
+            isFormValid = false;
+        }       
+        if (!isFormValid) {
+            return;
+        }
         onSubmit(formProps);
+        
     };
 
     return (
